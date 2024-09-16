3acd106e838d029ccccc5796e71191c8e5c88b02
Olive <59889200+Olive-Garden-official@users.noreply.github.com>
2024-09-15 22:27:35 -0600

    Moved js to external js

---
+++ index.js
@@ -0,0 +1,27 @@
+export default async (request, context) => {
+    const MY_APIKEY = Netlify.env.get("MY_APIKEY");
+    const MY_DATABASEURL = Netlify.env.get("MY_DATABASEURL");
+
+};
+require('dotenv').config()
+
+var deviceID = "000000001"
+var firebaseConfig = {
+    apiKey: MY_APIKEY,
+    databaseURL: MY_DATABASEURL,
+    projectId: "clicker-io-64f27"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+function clickEvent() {
+    console.log("click event");
+    event.preventDefault();
+    firebase.database().ref(deviceID).set({
+        clickBool: true,
+
+    });
+
+    // var button = document.getElementById('#myButton');
+    // button.setAttribute('disabled', '');
+}
